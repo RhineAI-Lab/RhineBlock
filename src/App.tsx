587c99a9499664864760c1a7726e8569177f9f7c
Murseth.Null <dongfengyao@yeah.net>
2022-08-22 01:14:32 +0800

    feat(utils): svg path元素接口封装

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
 import Flyout from "./components/base/Flyout/flyout";
+import SvgBuilder from "./core/utils/svg-builder";
 
 function App() {
+  const svgBuilder = new SvgBuilder();
+  const temp = svgBuilder
+    .moveTo(0, 0, true)
+    .lineTo(100, 0, true)
+    .build();
+  console.log(temp);
   return (
     <div className="App">
       <Flyout/>
